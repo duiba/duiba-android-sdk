834b2c588f7661d8f3efe09bdf5283c45f26f9fe
fxtayj <fxtayj@163.com>
2015-01-27 12:54:11 +0800

    banbenhao

--- src/cn/com/duiba/credits/CreditActivity.java
+++ src/cn/com/duiba/credits/CreditActivity.java
@@ -48,7 +48,7 @@ import android.widget.TextView;
  */
 public class CreditActivity extends Activity {
 
-	public static final String VERSION="1.0";
+	public static final String VERSION="1.0.1";
     private static String ua;
 
     public interface CreditsListener{
