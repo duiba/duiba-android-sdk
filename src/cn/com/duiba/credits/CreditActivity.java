7cba6391f0d6f8f18d11d728d4bf769b68dfb469
fxtayj <fxtayj@163.com>
2015-07-27 09:37:02 +0800

    修改版本号为固定格式x.x.x，固定接口，修改服务端实现。

--- src/cn/com/duiba/credits/CreditActivity.java
+++ src/cn/com/duiba/credits/CreditActivity.java
@@ -92,7 +92,7 @@ import android.widget.TextView;
 public class CreditActivity extends Activity {
 	private static String ua;
 	private static Stack<CreditActivity> activityStack;
-	public static final String VERSION="107";
+	public static final String VERSION="1.0.7";
     public static CreditsListener creditsListener;
 
     public interface CreditsListener{
